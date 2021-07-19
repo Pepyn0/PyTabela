d04af767dbd2a7d8740763223d62af06b1373898
Pepyn0 <brpepy@gmail.com>
2021-07-19 18:11:59 -0300

    Commit Inicial

---
+++ tests/main.py
@@ -0,0 +1,12 @@
+# import PyTabela
+
+# a = PyTabela()
+# a.addCampos(["id", "nome", "sobrenome"])
+# a.addLinha([1, "Pablo", "Silva"])
+# a.addLinha([2, "Vitória", "Karolina"])
+# a.addLinha([3, "batataaaaaaaa", "potato"])
+
+# a.saveTabela("teste.csv")
+
+# # a.loadTabela("teste.csv")
+# print(a)
